b48c62bca013ddc90c38252d96d6b5973f88317b
Teo Kuo Hong <kuohong@netizenexperience.com>
2023-06-25 00:56:20 +0800

    feat: vuetify setup

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
 
+//add Vuetify
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
