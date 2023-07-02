6586614bc21ed52d7cb238bfba0ea723bd45eafa
Teo Kuo Hong <kuohong@netizenexperience.com>
2023-07-02 18:34:12 +0800

    feat: completed loading spinner

---
+++ src/components/SpinnerComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="full-screen-spinner">
+    <v-progress-circular :size="50" color="primary" indeterminate></v-progress-circular>
+  </div>
+</template>
+
+<style scoped>
+.full-screen-spinner {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+</style>
